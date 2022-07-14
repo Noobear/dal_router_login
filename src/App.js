989b47ad3bc4b8932aa92537e6068eb55355107e
tmshdnaos45 <tmshdnaos45@naver.com>
2022-07-14 17:03:41 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+import { signIn } from './auth';
+import AuthRoute from './AuthRoute';
+
+import Home from './Home';
+import About from './About';
+import Profile from './Profile';
+import NotFound from './NotFound';
+import LoginForm from './LoginForm';
+import LogoutButton from './LogoutButton';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const authenticated = user != null;
+
+  const login = ({ username, password }) => setUser(signIn({ username, password }));
+  const logout = () => setUser(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <header>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/about">
+            <button>About</button>
+          </Link>
+          <Link to="/profile">
+            <button>Profile</button>
+          </Link>
+          {authenticated ? (
+              <LogoutButton logout={logout} />
+          ) : (
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+          )}
+        </header>
+        <hr />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route
+                path="/login"
+                render={props => (
+                    <LoginForm authenticated={authenticated} login={login} {...props} />
+                )}
+            />
+            <AuthRoute
+                authenticated={authenticated}
+                path="/profile"
+                render={props => <Profile user={user} {...props} />}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </Router>
   );
 }
 
